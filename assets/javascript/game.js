6eae984d6987356c5d3091b05316964d0ab4ed03
Kam Sah <ksahrakar@gmail.com>
2018-08-27 17:37:33 -0700

    some basic html

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+// Global Variables
+
+var brainWords=["commissure","hippocampus","ventricle","amygdala","cerebellum","splenium","chiasm","fornix","cortex","hypothalamus"];
+var numWins = 0;
+var numLosses = 0;
+
+// Functions
+// Display current word with visible guessed letters
+// Display letters guessed for current word
+// Display number of guesses remaining of 11
