a2629fbd93610112436fd21b0b697bf00ba6658e
Kamran Sahrakar <41097862+ksahrakar@users.noreply.github.com>
2018-08-29 13:11:10 -0700

    initial core

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,50 @@
-// Global Variables
+// Global variables
+var wordArray=["commissure","hippocampus","ventricle","amygdala","cerebellum","splenium","chiasm","fornix","cortex","hypothalamus"]
+var gameNumber = 0;
+var wins = 0;
+var losses = 0;
 
-var brainWords=["commissure","hippocampus","ventricle","amygdala","cerebellum","splenium","chiasm","fornix","cortex","hypothalamus"];
-var numWins = 0;
-var numLosses = 0;
 
-// Functions
-// Display current word with visible guessed letters
-// Display letters guessed for current word
-// Display number of guesses remaining of 11
+//Variables that exist for each game
+var currentWord = wordArray[gameNumber];
+var coverArray = [];
+var splitWordArray = [];
+var guessesLeft = 11;
+var lettersGuessed = [];
+
+// Split the mystery word into an array
+for (i=0;i<currentWord.length;i++){
+    splitWordArray[i] = currentWord.charAt(i);
+    coverArray[i]= "-";    
+}
+
+// Game starts once any key is pressed
+document.onkeypress= function(el){
+    //Make sure have guesses left
+    if (guessesLeft>0){
+        var choice = el.key.toLowerCase();
+        var inThere = currentWord.indexOf(choice);            
+        if (inThere>=0){
+
+            // Compare each array item to guessed letter
+            for (j=0;j<currentWord.length;j++){
+                if (splitWordArray[j]==choice){
+
+                    //Show letters in array that match guessed letter
+                    coverArray[j] = splitWordArray[j];
+                    console.log(coverArray);
+                }   
+            }
+        } else {
+
+            // Add letter to array of guessed letters
+                    lettersGuessed.push(choice);
+                    console.log(lettersGuessed);
+                    guessesLeft--;    
+        }
+    }else{
+        losses++;
+        alert("No more guesses left.\nYOU LOSE this game.");
+        confirm("Play again?")
+    }    
+}  
