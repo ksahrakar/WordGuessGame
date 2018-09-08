0eea3fce8c7036b680d8c43a9d1a2c726fb9393d
Kamran Sahrakar <41097862+ksahrakar@users.noreply.github.com>
2018-09-08 09:18:22 -0700

    loop attempts

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,12 @@
 // Global variables while playing game
 var wordArray = ["cortex", "ventricle", "cerebellum", "amygdala", "hippocampus", "splenium", "chiasm", "fornix", "commissure", "hypothalamus"]
-var hintImage = ["assets/images/C.jpg", "assets/images/V.jpg", "assets/images/Cebe.jpg","assets/images/AC.jpg", "assets/images/pi.jpg", "assets/images/F.jpg", "assets/images/SI.jpg"];
+var hintImage = ["assets/images/C.jpg", "assets/images/V.jpg", "assets/images/Cebe.jpg", "assets/images/y.jpg", "assets/images/pp.jpg", "assets/images/pi.jpg", "assets/images/sm.jpg", "assets/images/F.jpg", "assets/images/AC.jpg", "assets/images/po.jpg"];
 var gameNumber = 1;
 var wins = 0;
 var losses = 0;
-var wannaPlay = true;
-var stopPlaying = false;
 
-function playGame() {
-    //Variables that exist for each game
+
+function playGame(){
     var currentWord = wordArray[(gameNumber - 1)];
     var currentHintPic = hintImage[(gameNumber - 1)];
     var coverArray = [];
@@ -16,34 +14,29 @@ function playGame() {
     var guessesLeft = 10;
     var lettersGuessed = [];
     var choice;
-    // function resetGame(){
-    //     coverArray = [];
-    //     guessesLeft = 10;
-    //     lettersGuessed = [];
-    //     choice = "";
-    // }
 
-    //Show hintImage for current game NOT WORKING
-    console.log(currentHintPic);
-    //document.getElementById("hintImageH").src = currentHintPic;
+    //Show hintImage for current game
+    document.getElementById("hintImageH").src = currentHintPic;
 
-    // Split the mystery word into an array and show blank array so that player knows number of letters and show hint image WORKING
+    // Split the mystery word into an array
     for (i = 0; i < currentWord.length; i++) {
         splitWordArray[i] = currentWord.charAt(i);
         coverArray[i] = "|_|";
     };
-    console.log(coverArray);
-    //document.getElementById("currentWordH").innerHTML = coverArray.join(" ");
-    //
-    
-    //}
+
+    //Show blank array so that player knows number of letters
+    document.getElementById("currentWordH").innerHTML = coverArray.join(" ");
+
     // Game starts once any key is pressed
-    document.onkeypress = function (el) {
-        
+    
+    document.onkeypress = function(el) {
+
         //Make sure have guesses left
         if (guessesLeft > 0) {
+
+            //get key choice
             choice = el.key.toLowerCase();
-            console.log(choice);
+            
             if (currentWord.includes(choice)) {
 
                 // Compare each array item to guessed letters
@@ -54,7 +47,7 @@ function playGame() {
                         coverArray[j] = splitWordArray[j];
                         var joinedWorking = coverArray.join(" ");
                         document.getElementById("currentWordH").innerHTML = joinedWorking.toUpperCase();
-                    };
+                    }
                 };
 
                 //End game if all letters are guessed
@@ -63,51 +56,51 @@ function playGame() {
                 if (joinedCover == joinedCurrent) {
                     wins++;
                     document.getElementById("winsH").innerHTML = "WINS: " + wins;
-                    alert("You WIN!");
-                    wannaPlay = confirm("Play another game?");
-                    if (wannaPlay) {
+                    alert("You guessed the part!\nYou win!!");
+                    answer = confirm("Play another?");
+                    if (answer){
                         gameNumber++;
-                        //currentWord = wordArray[(gameNumber - 1)];
-                        // coverArray = [];
-                        // guessesLeft = 10;
-                        // lettersGuessed = [];
-                        // choice = "";
+                        return;
                     } else {
-                        stopPlaying = true;
-                    };
-                };
+                        gameNumber = 12;
+                        return;
+                    }
+                }
 
             } else {
+
+                //Add letter to list of guessed letters
                 lettersGuessed.push(choice);
                 document.getElementById("lettersGuessedH").innerHTML = lettersGuessed;
                 guessesLeft--;
                 document.getElementById("guessesLeftH").innerHTML = "GUESSES LEFT: " + guessesLeft;
-            };
+            }
 
-        } else {
+        } else {   
+            alert("No more guesses left.\nYou LOSE this game.");
             losses++;
             document.getElementById("lossesH").innerHTML = "LOSSES: " + losses;
-            alert("No more guesses left.\nYou LOSE this game.");
             document.getElementById("currentWordH").innerHTML = splitWordArray.join(" ").toUpperCase();
-            wannaPlay = confirm("Play another game?");
-            if (wannaPlay) {
+            answer = confirm("Play another?");
+            if (answer){
                 gameNumber++;
-                //currentWord = wordArray[(gameNumber - 1)];
-                // coverArray = [];
-                // guessesLeft = 10;
-                // lettersGuessed = [];
-                // choice = "";
+                return;
             } else {
-                stopPlaying = true;
-            };
-        };
-    };
+                gameNumber = 12;
+                return;
+            }
+        }
+    }
+}
+while ((gameNumber>=1) && (gameNumber<11)){
+    playGame();
+}
+
+if (gameNumber == 11){
+    alert("You've finished the game");
+}
+if (gameNumber == 12){
+    alert("Thanks for playing");
 };
 
-// Repeat game as indicated NOT WORKING PROPERLY
 
-if(!stopPlaying){
-    playGame();
-} else {
-    alert("We're done here!");
-}
